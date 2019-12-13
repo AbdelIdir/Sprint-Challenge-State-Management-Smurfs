edbfd093a63474e824e3f3bae1cf035d72a8a865
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-13 12:14:41 +0100

    refined styling

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -4,7 +4,18 @@ import Form from "./Form";
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ background: "silver", height: "100vh" }}>
+      <div className="App" style={{ background: "silver", height: "100%" }}>
+        <br />
+        <span
+          style={{
+            marginBottom: "80px",
+            fontWeight: "bold",
+            fontSize: "40px",
+            paddingBottom: "90px"
+          }}
+        >
+          Let's make a smurf Village !
+        </span>
         <Form />
       </div>
     );
